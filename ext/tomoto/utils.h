9568f7e20b80f1aaf3c2659b384e09f9df0002b8
Andrew Kane <andrew@ankane.org>
2021-04-29 23:41:23 -0700

    Updated Rice

--- ext/tomoto/utils.h
+++ ext/tomoto/utils.h
@@ -1,93 +1,24 @@
 #pragma once
 
 #include <rice/rice.hpp>
+#include <rice/stl.hpp>
 
 using Rice::Array;
 using Rice::Object;
 
 namespace Rice::detail
 {
-  template<>
-  struct To_Ruby<std::vector<tomoto::Float>>
+  template<typename T>
+  class To_Ruby<std::vector<T>>
   {
-    static VALUE convert(std::vector<tomoto::Float> const & x, bool takeOwnership = false)
+  public:
+    VALUE convert(std::vector<T> const & x)
     {
-      Array res;
-      for (auto const& v : x) {
-        res.push(v);
+      auto a = rb_ary_new2(x.size());
+      for (const auto& v : x) {
+        protect(rb_ary_push, a, To_Ruby<T>().convert(v));
       }
-      return res;
-    }
-  };
-
-  template<>
-  struct To_Ruby<std::vector<uint32_t>>
-  {
-    static VALUE convert(std::vector<uint32_t> const & x, bool takeOwnership = false)
-    {
-      Array res;
-      for (auto const& v : x) {
-        res.push(v);
-      }
-      return res;
-    }
-  };
-
-  template<>
-  struct To_Ruby<std::vector<uint64_t>>
-  {
-    static VALUE convert(std::vector<uint64_t> const & x, bool takeOwnership = false)
-    {
-      Array res;
-      for (auto const& v : x) {
-        res.push(v);
-      }
-      return res;
-    }
-  };
-
-  template<>
-  struct From_Ruby<std::vector<std::string>>
-  {
-    static std::vector<std::string> convert(VALUE x)
-    {
-      Array a = Array(x);
-      std::vector<std::string> res;
-      res.reserve(a.size());
-      for (auto const& v : a) {
-        res.push_back(Rice::detail::From_Ruby<std::string>::convert(v.value()));
-      }
-      return res;
-    }
-  };
-
-  template<>
-  struct From_Ruby<std::vector<tomoto::Float>>
-  {
-    static std::vector<tomoto::Float> convert(VALUE x)
-    {
-      Array a = Array(x);
-      std::vector<tomoto::Float> res;
-      res.reserve(a.size());
-      for (auto const& v : a) {
-        res.push_back(Rice::detail::From_Ruby<tomoto::Float>::convert(v.value()));
-      }
-      return res;
-    }
-  };
-
-  template<>
-  struct From_Ruby<std::vector<uint64_t>>
-  {
-    static std::vector<uint64_t> convert(VALUE x)
-    {
-      Array a = Array(x);
-      std::vector<uint64_t> res;
-      res.reserve(a.size());
-      for (auto const& v : a) {
-        res.push_back(Rice::detail::From_Ruby<uint64_t>::convert(v.value()));
-      }
-      return res;
+      return a;
     }
   };
 }
